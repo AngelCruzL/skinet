467d0240bc8a7c1d63d4a7153e1ccd2e4722a9ec
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-04-10 17:30:32 -0600

    Add repository pattern to Product controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,7 +1,6 @@
 using Core.Entities;
-using Infrastructure.Data;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -9,22 +8,24 @@ namespace API.Controllers;
 [Route("api/[controller]")]
 public class ProductsController : ControllerBase
 {
-  private readonly StoreContext _context;
+  private readonly IProductRepository _repository;
 
-  public ProductsController(StoreContext context)
+  public ProductsController(IProductRepository repository)
   {
-    _context = context;
+    _repository = repository;
   }
 
   [HttpGet]
   public async Task<ActionResult<List<Product>>> GetProducts()
   {
-    return await _context.Products.ToListAsync();
+    var products = await _repository.GetProductsAsync();
+
+    return Ok(products);
   }
 
   [HttpGet("{id}")]
   public async Task<ActionResult<Product>> GetProduct(int id)
   {
-    return await _context.Products.FindAsync(id);
+    return await _repository.GetProductByIdAsync(id);
   }
 }
