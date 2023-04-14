7cecf0d69f4073ebccfbbd5299fe5225a0c04bc1
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-04-14 16:02:41 -0600

    [client] Display product names on homepage

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: any;
+  #http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.#http
+      .get('https://localhost:5001/api/products?pageSize=50')
+      .subscribe((response: any) => {
+        this.products = response.data;
+      });
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { CoreModule } from 'src/app/core/core.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CoreModule } from 'src/app/core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,6 +14,7 @@ import { CoreModule } from 'src/app/core/core.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     CoreModule,
   ],
   providers: [],
