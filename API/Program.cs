3b34b2d3623b9302f713a0b1d274a2a05c7f0d59
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-04-14 10:41:15 -0600

    Remove limitation to generate swagger documentation in production mode

--- API/Program.cs
+++ API/Program.cs
@@ -48,11 +48,8 @@ app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
-if (app.Environment.IsDevelopment())
-{
-  app.UseSwagger();
-  app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseStaticFiles();
 
