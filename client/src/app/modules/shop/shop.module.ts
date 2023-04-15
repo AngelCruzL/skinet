fd1bd69de657c1d7db847e26924756d2b1bf2d91
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-04-14 18:32:13 -0600

    [client] Fix product-item component directory and add margin on title

--- client/src/app/modules/shop/shop.module.ts
+++ client/src/app/modules/shop/shop.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ShopRoutingModule } from './shop-routing.module';
+
 import { ShopComponent } from './shop.component';
-import { ProductItemComponent } from './pages/product-item/product-item.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
 
 @NgModule({
   declarations: [ShopComponent, ProductItemComponent],
