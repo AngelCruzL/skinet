b3ba7dcbcf9ec1e5cd879b95ba79af9fd0add6cf
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-04-17 12:55:14 -0600

    [client] Add home page route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () =>
+      import('./modules/home/home.module').then(m => m.HomeModule),
+  },
+  {
     path: 'shop',
     loadChildren: () =>
       import('./modules/shop/shop.module').then(m => m.ShopModule),
