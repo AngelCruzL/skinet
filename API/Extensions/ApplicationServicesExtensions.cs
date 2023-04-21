25453619f14ae5ea738179ff45ed1a4609ca3a69
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-04-21 11:57:40 -0600

    Implement redis and Basket repository

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -22,9 +22,10 @@ public static class ApplicationServicesExtensions
     // Redis config
     services.AddSingleton<IConnectionMultiplexer>(c =>
     {
-      var options = ConfigurationOptions.Parse(config.GetConnectionString("Redis"));
+      var options = ConfigurationOptions.Parse(config.GetConnectionString("Redis")!);
       return ConnectionMultiplexer.Connect(options);
     });
+    services.AddScoped<IBasketRepository, BasketRepository>();
 
     services.AddScoped<IProductRepository, ProductRepository>();
     services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
